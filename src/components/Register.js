80e63a9286e497b955c5dca1de74be66ac38f37d
Wojciech Rithaler <writhaler@gmail.com>
2019-08-27 22:58:26 +0200

    small changes, still no validation

--- src/components/Register.js
+++ src/components/Register.js
@@ -4,6 +4,7 @@ import '../App.css';
 import Axios from 'axios';
 
 import { UserContext, UserLoginContext } from '../App';
+import useInput from '../hooks/useInput';
 
 function Register() {
   const user = useContext(UserContext);
@@ -18,22 +19,19 @@ function Register() {
 
     const [input, setInput] = useState({
         login: "",
-        loginError: "",
-
         email: "",
         password: "",
         password2: "",
     });
 
-    const handleState = (event) => {
-        const value = event.target.value;
-        setInput({...input, [event.target.name]: value});
+    const handleChange = (event) => {
+        const {name, value} = event.target;
+        setInput({...input, [name]: value});
     }
 
-    const login = (event) => {
+    const handleSubmit = (event) => {
         
         event.preventDefault();
-        //console.log(input);
 
         Axios.post('http://localhost:5000/api/user/register', input).then(res => {
             //console.log(res.data);
@@ -49,20 +47,20 @@ function Register() {
             <div className="card-header">
                 <h1>Login</h1>
             </div>
-            <form className="card-body">
-                <input type="text" className="form-control" placeholder="Enter your login" name="login" value={input.login} onChange={e => handleState(e)}></input><br />
+            <form className="card-body"> {/* ${error.login && 'is-invalid'} */}
+                <input type="text" className={`form-control`} placeholder="Enter your login" name="login" value={input.login} onChange={e => handleChange(e)}></input><br />
                 <br />
 
-                <input type="text" className="form-control" placeholder="Enter your email" name="email" value={input.email} onChange={e => handleState(e)}></input><br />
+                <input type="text" className="form-control" placeholder="Enter your email" name="email" value={input.email} onChange={e => handleChange(e)}></input><br />
                 <br />
 
-                <input type="text" className="form-control" placeholder="Enter your password" name="password" value={input.password} onChange={e => handleState(e)}></input><br />
+                <input type="text" className="form-control" placeholder="Enter your password" name="password" value={input.password} onChange={e => handleChange(e)}></input><br />
                 <br />
 
-                <input type="text" className="form-control" placeholder="Repeat the password" name="password2" value={input.password2} onChange={e => handleState(e)}></input><br />
+                <input type="text" className="form-control" placeholder="Repeat the password" name="password2" value={input.password2} onChange={e => handleChange(e)}></input><br />
                 <br />
 
-                <button id="submit" className="btn btn-primary" onClick={e => login(e)}>Register</button>
+                <button type="submit" className="btn btn-primary" onClick={e => handleSubmit(e)}>Register</button>
                 <br />
             </form>
             <div className="card-footer text-muted">
