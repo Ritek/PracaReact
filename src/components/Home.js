ed818f8055e9b775e517f33b722b3cdb4b0751a0
Wojciech <writhaler@gmail.com>
2019-09-27 22:09:25 +0200

    Added react-bootstrap dependency Createt a simple Modal

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,26 @@
 import React, {useContext} from 'react';
 import '../App.css';
 import {BrowserRouter as Router, Redirect} from 'react-router-dom';
+import Modal from 'react-bootstrap/Modal';
 
 function About() {
 
   return (
     <div>
       <h1>Home</h1>
+      <Modal.Dialog>
+        <Modal.Header closeButton>
+          <Modal.Title>Header</Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body>
+          <p>A modal body</p>
+        </Modal.Body>
+
+        <Modal.Footer>
+          <p>A footer</p>
+        </Modal.Footer>
+      </Modal.Dialog>
     </div>
   );
 }
