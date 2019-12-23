51527fbf455c7465dca61df0a24eb08e0dc312a1
Wojciech <writhaler@gmail.com>
2019-12-23 22:40:06 +0100

    styled buttons in dashboard

--- src/components/protected/TeacherDashboard.js
+++ src/components/protected/TeacherDashboard.js
@@ -1,33 +1,49 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 
+import './userDetails.css'
+
 function TeacherDashboard() {
-
-    const tile = {
-        minWidth: '300px',
-        minHeigth: '100px',
-
-        backgroundColor: 'gray',
-        height: '25vh',
-
-        marginBottom: '20px',
-        marginRight: '10px',
-        marginLeft: '10px'
-    }
-
     return (
         <div>
             <div className="row">
-                <Link className="col-sm" style={tile} to='/user/creategroup'>Create group</Link><br />
-                <Link className="col-sm" style={tile} to='/user/menagegroups'>Menage groups</Link><br />
+                <Link className="col-sm tile" to='/user/creategroup'>
+                    <div className="box">
+                        <i className="fas fa-cloud image"></i><br/>
+                        <p className="link">Create group</p>
+                    </div>
+                </Link><br />
+                <Link className="col-sm tile" to='/user/menagegroups'>
+                    <div className="box">
+                        <i className="fas fa-cloud image"></i><br/>
+                        <p className="link">Menage groups</p>
+                    </div>
+                </Link><br />
 
                 <div className="w-100" />
 
-                <Link className="col-sm" style={tile} to='/user/createtest'>Create test</Link><br />
-                <Link className="col-sm" style={tile} to='/user/testlist'>Your tests</Link><br />
+                <Link className="col-sm tile" to='/user/createtest'>
+                    <div className="box">
+                        <i className="fas fa-cloud image"></i><br/>
+                        <p className="link">Create test</p>
+                    </div>
+                </Link><br />
+                <Link className="col-sm tile" to='/user/testlist'>
+                    <div className="box">
+                        <i className="fas fa-cloud image"></i><br/>
+                        <p className="link">Your tests</p>
+                    </div>
+                </Link><br />
 
                 <div className="w-100" />
-                <Link className="col-sm" style={tile} to='/user/allsolvedtests'>Solved Tests</Link>
+
+                <Link className="col-sm tile" to='/user/allsolvedtests'>
+                    <div className="box">
+                        <i className="fas fa-cloud image"></i><br/>
+                        <p className="link">Solved Tests</p>
+                    </div>
+                </Link>
+
             </div>
         </div>
     )
